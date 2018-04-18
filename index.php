e68f294f36a7cbc64664c6b4b228b5a8d0178feb
jeje9 <jeje9@JRM98>
2018-04-17 21:59:09 -0400

    premier commit ppe4

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+    include_once 'vues/entetehtml.html';
+    include_once 'vues/entetepage.html';
+    include_once 'vues/pageaccueil.php';
+    include_once 'vues/piedpage.html';                            
+    include_once 'vues/piedhtml.html';
+?>
